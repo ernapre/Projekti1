847cb12e596464996bbf95faa155360e533c1b50
malisadiku <malisadiku3@gmail.com>
2024-02-03 19:43:34 +0100

    connection/errors/server

---
+++ errors.php
@@ -0,0 +1,22 @@
+<?php  if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
+
+<style>
+.error {
+  width: 30%; 
+  margin-left: 200px;
+  margin-top: -50px;
+  margin-bottom: -30px;
+  padding: 10px; 
+  border: 1px solid #a94442; 
+  color: #a94442; 
+  background: #f2dede; 
+  border-radius: 5px; 
+  text-align: left;
+}
+</style>
